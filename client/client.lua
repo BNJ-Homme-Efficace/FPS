99f8975f9d7bc3f6d28395bf3d8187910f213992
BNJ-Homme-Efficace <55634259+BNJ-Homme-Efficace@users.noreply.github.com>
2022-09-02 17:13:06 +0200

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -17,7 +17,7 @@ end)
 			RageUI.Button('Hight', '',  { RightBadge = RageUI.BadgeStyle.Star},  true, {
 				onSelected = function()
                     print("^6Woah merci BNJ")
-                    SetTimecycleModifier('MP_Powerplay_blend')
+                    SetTimecycleModifier('MP_casino_apartment_changing')
                     SetExtraTimecycleModifier('reflection_correct_ambient')
 				end,
 			});
@@ -77,7 +77,64 @@ end)
                     Citizen.Wait(300)
                 end,
             });
-    
+            RageUI.Separator("~r~Optimisation + FPS")
+            RageUI.Button('LOW avec optimisation', '',  { RightBadge = RageUI.BadgeStyle.Star},  true, {
+                onSelected = function()
+		            print("^6Woah merci BNJ")
+                    --------------------------------------------------------------------------------
+                    --------------------------------------OPTIMISATON-------------------------------
+                    --------------------------------------------------------------------------------
+                    if not HasModelLoaded(modelHash) then
+                    RequestModel(modelHash)BeginTextCommandBusyString('STRING')
+                    AddTextComponentSubstringPlayerName(('Optimisation 10%'))
+                    EndTextCommandBusyString(4)
+                    Citizen.Wait(5000)
+                    RemoveLoadingPrompt()
+                    ClearPedInPauseMenu() --Je clear
+                    ClearHelp()
+                    ClearNotificationsPos()
+                    RequestModel(modelHash)BeginTextCommandBusyString('STRING')
+                    AddTextComponentSubstringPlayerName(('Optimisation 50%'))
+                    EndTextCommandBusyString(4)
+                    Citizen.Wait(3000)
+                    RemoveLoadingPrompt()
+                    ClearReminderMessage() --Je Clear	
+                    ClearFloatingHelp()
+                    ClearGpsPlayerWaypoint()
+                    ClearGpsRaceTrack()
+                    ClearThisPrint()
+                    RequestModel(modelHash)BeginTextCommandBusyString('STRING')
+                    AddTextComponentSubstringPlayerName(('Optimisation 100%'))
+                    EndTextCommandBusyString(4)
+                    Citizen.Wait(4000)
+                    RemoveLoadingPrompt()
+                    ExecuteCommand("clear")
+                    end
+                    SetTimecycleModifier('yell_tunnel_nodirect')
+                    ClearAllBrokenGlass()
+                    ClearAllHelpMessages()
+                    LeaderboardsReadClearAll()
+                    ClearBrief()
+                    ClearGpsFlags()
+                    ClearPrints()
+                    ClearSmallPrints()
+                    ClearReplayStats()
+                    LeaderboardsClearCacheData()
+                    ClearFocus()
+                    ClearHdArea()
+                    ClearPedBloodDamage(PlayerPedId())
+                    ClearPedWetness(PlayerPedId())
+                    ClearPedEnvDirt(PlayerPedId())
+                    ResetPedVisibleDamage(PlayerPedId())
+                    ClearOverrideWeather()
+                    DisableVehicleDistantlights(false)
+                    DisableScreenblurFade()
+                    SetRainLevel(0.0)
+                    SetWindSpeed(0.0)
+                    Citizen.Wait(300)
+                end,
+            });
+            RageUI.Separator("~r~Par default")
                 RageUI.Button('Res', '',  { RightBadge = RageUI.BadgeStyle.Star},  true, {
                     onSelected = function()
                         print("^6Woah merci BNJ")
@@ -97,4 +154,3 @@ end)
         end
     end
 end)
-
