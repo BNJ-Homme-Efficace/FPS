fda79c898d6fd18ff2c32230e8f9c3df0e171a9b
BNJ-Homme-Efficace <55634259+BNJ-Homme-Efficace@users.noreply.github.com>
2022-09-02 21:08:36 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,6 @@
 fx_version 'adamant'
 games { 'gta5' };
-version '1.2'
+version '1.3'
 
 name 'RageUI';
 description 'RageUI, and a project specially created to replace the NativeUILua-Reloaded library. This library allows to create menus similar to the one of Grand Theft Auto online.'
