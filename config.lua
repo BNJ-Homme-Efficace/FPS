a9047eed73bf2ba5be3dd89eb4bb3485ee2593d2
BNJ-Homme-Efficace <55634259+BNJ-Homme-Efficace@users.noreply.github.com>
2022-09-02 21:07:59 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,15 @@
+Strings = {
+    ['settings'] = 'Menu FPS',
+    ['menufps'] = '~r~Menu FPS~s~',
+    ['low'] = 'Low',
+    ['med'] = 'Medium',
+    ['hight'] = 'Hight',   
+    ['opti'] = '~r~Optimisation + FPS~s~',   
+    ['superopti'] = 'Low with optimisation',   
+    ['rest'] = 'Restore',
+    ['default'] = '~r~By default~s~',   
+    ['opti1'] = 'Optimisation 10%',   
+    ['opti2'] = 'Optimisation 50%',   
+    ['opti3'] = 'Optimisation 100%%',   
+    
+  }
